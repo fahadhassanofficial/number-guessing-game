213715854a55a086eea991a94471e7614e92a350
fahadhassanofficial <fahad.hassan@consultant.com>
2024-04-03 12:28:28 +0500

    version 1.0.1

--- code.js
+++ code.js
@@ -1,13 +1,14 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-//1 computer generate random number.
-//2 user input for guessing number.
-//3compare user input with computer generated number and show result.
-const randonNumber = 13;
+//1 computer generate random number. done
+//2 user input for guessing number. done
+//3compare user input with computer generated number and show result. done
+const randonNumber = Math.floor(Math.random() * 10 + 1);
 const answer = await inquirer.prompt([
     {
         name: "Numbering",
         type: "number",
-        message: "please guess number",
+        message: "please guess number between 1-10",
     }
 ]);
 if (answer.Numbering === randonNumber) {
--- code.ts
+++ code.ts
@@ -1,17 +1,20 @@
+#! /usr/bin/env node
+
+
 import inquirer from "inquirer";
-//1 computer generate random number.
+//1 computer generate random number. done
 
-//2 user input for guessing number.
+//2 user input for guessing number. done
 
-//3compare user input with computer generated number and show result.
-const randonNumber = 13;
+//3compare user input with computer generated number and show result. done
+const randonNumber = Math.floor(Math.random() * 10 + 1);
 
 const answer = await inquirer.prompt([
 
 {
     name: "Numbering",
     type: "number",
-    message: "please guess number",
+    message: "please guess number between 1-10",
 
 }
 ]);
